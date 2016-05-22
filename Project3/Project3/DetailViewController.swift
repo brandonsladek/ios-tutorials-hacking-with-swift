3e3995e6b00f137cbea9251b79975637ee9077a3
brandonsladek <brandonsladek@gmail.com>
2016-05-22 00:21:30 -0600

    File paths a little messed up but oh well...

---
+++ Project3/Project3/DetailViewController.swift
@@ -0,0 +1,74 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Hudzilla on 13/09/2015.
+//  Copyright © 2015 Paul Hudson. All rights reserved.
+//
+
+import UIKit
+import Social
+
+class DetailViewController: UIViewController {
+
+
+	@IBOutlet weak var detailImageView: UIImageView!
+
+
+	var detailItem: String? {
+		didSet {
+		    // Update the view.
+		    self.configureView()
+		}
+	}
+
+	func configureView() {
+		// Update the user interface for the detail item.
+		if let detail = self.detailItem {
+			if let imageView = self.detailImageView {
+				imageView.image = UIImage(named: detail)
+			}
+            navigationItem.title = detail
+		}
+        
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		self.configureView()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: #selector(shareTappedTwitter))
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	override func viewWillAppear(animated: Bool) {
+		super.viewWillAppear(animated)
+		navigationController?.hidesBarsOnTap = true
+	}
+
+	override func viewWillDisappear(animated: Bool) {
+		super.viewWillDisappear(animated)
+		navigationController?.hidesBarsOnTap = false
+	}
+    
+    func shareTappedManyChoices() {
+        let vc = UIActivityViewController(activityItems: [detailImageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    func shareTappedTwitter() {
+        let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        vc.setInitialText("Look at this great picture!")
+        vc.addImage(detailImageView.image!)
+        vc.addURL(NSURL(string: "http://www.photolib.noaa.gov/nssl"))
+        presentViewController(vc, animated: true, completion: nil)
+    }
+    
+}
+
